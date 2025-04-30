c8b8df30549445cf74d7f820fd9573017fd12c2d
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-04-30 13:07:57 +0200

    feat

--- P_Passion_Lecture/MainPage.xaml.cs
+++ P_Passion_Lecture/MainPage.xaml.cs
@@ -1,13 +1,36 @@
-﻿namespace P_Passion_Lecture
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Layouts;
+
+
+
+namespace P_Passion_Lecture
 {
     public partial class MainPage : ContentPage
     {
+        public string FictionFantastique {  get; set; } 
+        public string Romance {  get; set; }
+        public string MystereSuspence {  get; set; }
+        public string Horreur {  get; set; }
+        public string ScienceTechnologie {  get; set; }
+        public string Aventure {  get; set; }
+        public string DeveloppementPersonnel {  get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+
+            FictionFantastique = "Fiction Fantastique";
+            Romance = "Romance";
+            MystereSuspence = "Mystère & Suspence";
+            Horreur = "Horreur";
+            ScienceTechnologie = "Science & Technologie";
+            Aventure = "Aventure";
+            DeveloppementPersonnel = "Développement Personnel";
+
+            BindingContext = this;
         }
-
-
     }
 
 }
+
+
